48f6d5dc471db328895758fdc9b25fad106fe2c4
wescorner <58126085+wescorner@users.noreply.github.com>
2021-05-21 00:22:47 -0400

    starting point get requests
    creating the root get request and the city get request, which currently only returns the city name entered

--- app.js
+++ app.js
@@ -3,3 +3,21 @@ const bodyParser = require ('body-parser');
 
 //initialize express app
 const app = express();
+const port = 3000;
+
+//open the port to listening
+app.listen(port, () => {
+    console.log('Server is up and running on port ' + port);
+});
+
+//root
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+//creating the city inquiry get request
+app.get('/api/city/:name', (req, res) => {
+    const name = req.params.name;
+    res.send(name);
+    console.log(`GET request for ${req.url}`);
+})
