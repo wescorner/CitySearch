9bb2c795928e475c3cdd50f1a007206411a67b7b
wescorner <58126085+wescorner@users.noreply.github.com>
2021-05-26 22:48:12 -0400

    return needed info on city get request
    when making a get request for a city, the information we need for currency/timezone/weather are only the latitude, longitude, and iso3. So these 3 parameters are returned on city get request

--- Server/src/index.js
+++ Server/src/index.js
@@ -18,7 +18,21 @@ app.get('/', (req, res) => {
 
 //creating the city inquiry get request
 app.get('/api/city/:name', (req, res) => {
-    const name = req.params.name;
-    res.send(name);
     console.log(`GET request for ${req.url}`);
+    const name = req.params.name;
+
+    //we want to grab the object from the cities json with the specified name
+    //then return that city's iso3, lat, and lng
+
+    var cityinfo = [];
+
+    for (var i = 0; i < worldcities.length; i++){
+        if(worldcities[i]["city"] == name){
+            cityinfo.push(worldcities[i]["lat"]);
+            cityinfo.push(worldcities[i]["lng"]);
+            cityinfo.push(worldcities[i]["iso3"]);
+        }
+    }
+    res.send(cityinfo);
+    
 })
