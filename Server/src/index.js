c20a8a166d5bc0c7e655aa90f52e722d73b3495f
wescorner <58126085+wescorner@users.noreply.github.com>
2021-05-27 00:56:20 -0400

    cleanup and timezone api
    some commenting and cleanup, as well as adding the request to call the google timezone API

--- Server/src/index.js
+++ Server/src/index.js
@@ -18,12 +18,16 @@ app.listen(port, () => {
 //currency API
 //https://v6.exchangerate-api.com/v6/c062528abc5d3fae4044a83d/latest/CAD
 
+//timezone API
+//https://maps.googleapis.com/maps/api/timezone/json?location=LAT,LNG&timestamp=0&key=AIzaSyAD3EPT5bdU6tzanFyeBhpOgIKuAj8cg1U
+
 currency = [];
 
+//make the currency API call and push resulting object to currency array
 request('https://v6.exchangerate-api.com/v6/c062528abc5d3fae4044a83d/latest/CAD', function(error, response, body) {
-    currency.push(JSON.parse(body));
-    
-})
+    currency.push(JSON.parse(body)); 
+});
+
 
 
 //root
@@ -38,11 +42,16 @@ app.get('/api/city/:name', (req, res) => {
 
     //declaring variables
     var cityinfo = [];
+    var timezoneinfo = [];
     var country;
     var lat;
     var lng;
     var currencycode;
     var conversionrate;
+    var seconds;
+
+    var date = new Date();//new date object
+    seconds = Math.round(date.getTime() / 1000);//getting the amount of seconds since epoch, to use on timezone API call
 
     //this will find the city in the json file and grab the country name, latitude, and longitude values
     for (var i = 0; i < worldcities.length; i++){
@@ -63,6 +72,15 @@ app.get('/api/city/:name', (req, res) => {
 
     conversionrate = currency[0]["conversion_rates"][currencycode];//this will use the array from the API to get conversion rate from the currency code
     cityinfo.push(conversionrate);//push the conversion rate to the array
-    res.send(cityinfo);//send the array to user
+   
+    //make the timezone API call to get timezone and epoch of specified lat/lng
+    /*
+    request(`https://maps.googleapis.com/maps/api/timezone/json?location=${lat},${lng}&timestamp=${seconds}&key=AIzaSyAD3EPT5bdU6tzanFyeBhpOgIKuAj8cg1U`, function(error, response, body) {
+        timezoneinfo.push(JSON.parse(body));
+    });
+    */
     
-})
+    
+    
+    res.send(cityinfo);//send the array to user  
+});
