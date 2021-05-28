b0f44fb24ba39eea3491eedb08ca6cac5692a80e
wescorner <58126085+wescorner@users.noreply.github.com>
2021-05-28 00:56:04 -0400

    calculating local time
    introduced all the logic for calculating the current seconds since epoch, using that in API call parameters to calculate local time and push that to the array that is sent to the user

--- Server/src/index.js
+++ Server/src/index.js
@@ -28,8 +28,6 @@ request('https://v6.exchangerate-api.com/v6/c062528abc5d3fae4044a83d/latest/CAD'
     currency.push(JSON.parse(body)); 
 });
 
-
-
 //root
 app.get('/', (req, res) => {
     res.send('Hello World');
@@ -42,16 +40,18 @@ app.get('/api/city/:name', (req, res) => {
 
     //declaring variables
     var cityinfo = [];
-    var timezoneinfo = [];
+    //var timezoneinfo = [];
     var country;
     var lat;
     var lng;
     var currencycode;
     var conversionrate;
     var seconds;
+    var dstoffset;
+    var rawoffset;
 
     var date = new Date();//new date object
-    seconds = Math.round(date.getTime() / 1000);//getting the amount of seconds since epoch, to use on timezone API call
+    seconds = Math.round((date.getTime() + (date.getTimezoneOffset() * 60000))/ 1000);//getting the amount of seconds since epoch, to use on timezone API call
 
     //this will find the city in the json file and grab the country name, latitude, and longitude values
     for (var i = 0; i < worldcities.length; i++){
@@ -74,13 +74,44 @@ app.get('/api/city/:name', (req, res) => {
     cityinfo.push(conversionrate);//push the conversion rate to the array
    
     //make the timezone API call to get timezone and epoch of specified lat/lng
-    /*
-    request(`https://maps.googleapis.com/maps/api/timezone/json?location=${lat},${lng}&timestamp=${seconds}&key=AIzaSyAD3EPT5bdU6tzanFyeBhpOgIKuAj8cg1U`, function(error, response, body) {
-        timezoneinfo.push(JSON.parse(body));
+
+    
+
+
+    //need to make a promise chain:
+    //first will retrieve offset
+    //then will calculate local time based on offset result
+    //then will retreive weather information
+    //then will res.send the final result with everything
+    new Promise(function(resolve, reject){
+        console.log("number 1 ran");
+        //retreive offset
+        request(`https://maps.googleapis.com/maps/api/timezone/json?location=${lat},${lng}&timestamp=${seconds}&key=AIzaSyAD3EPT5bdU6tzanFyeBhpOgIKuAj8cg1U`, function(error, response, body) {
+        var timezoneinfo = (JSON.parse(body));
+        dstoffset = timezoneinfo["dstOffset"];
+        rawoffset = timezoneinfo["rawOffset"];
+        console.log(body); 
+        resolve(1000*(dstoffset + rawoffset + seconds));//sum offsets to take daylight savings into account
+        });
+    }).then(function(result){
+        
+        var nd = new Date(result);
+        cityinfo.push(nd.toLocaleString());
+
+    }).then(function(){
+        console.log("number 3 ran");
+        //res send 
+        res.send(cityinfo);
     });
+
+    /*
+    function calcTime(offset){
+        var d = new Date();
+        var utc = d.getTime() + (d.getTimezoneOffset() * 60000);
+        var nd = new Date(utc + (3600000 * offset));
+        cityinfo.push(nd.toLocaleDateString());
+    }
+    calcTime(offset);
     */
     
-    
-    
-    res.send(cityinfo);//send the array to user  
 });
