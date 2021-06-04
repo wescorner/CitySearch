0d6a3a056678e057d6ad98441d3695abd4394df3
wescorner <58126085+wescorner@users.noreply.github.com>
2021-06-04 15:39:17 -0400

    adding the main landing page and creating styling
    adding the main page component and creating the styling for the background, font, and title

--- App/src/app/app-routing.module.ts
+++ App/src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CitySearchComponent } from './pages/city-search/city-search.component';
+import { MainPageComponent } from './pages/main-page/main-page.component';
 
 const routes: Routes = [
-  { path: '', component: CitySearchComponent }
+  { path: '', component: MainPageComponent }
 ];
 
 @NgModule({
--- App/src/app/app.module.ts
+++ App/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CitySearchComponent } from './pages/city-search/city-search.component';
+import { MainPageComponent } from './pages/main-page/main-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CitySearchComponent
+    CitySearchComponent,
+    MainPageComponent
   ],
   imports: [
     BrowserModule,
