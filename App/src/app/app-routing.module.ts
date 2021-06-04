029806843f7b1d68dc12059db80f498407242ac5
wescorner <58126085+wescorner@users.noreply.github.com>
2021-06-04 01:29:39 -0400

    creating component for city search
    creating the component to be used for searching cities and setting up the routing for that component

--- App/src/app/app-routing.module.ts
+++ App/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CitySearchComponent } from './pages/city-search/city-search.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: CitySearchComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- App/src/app/app.module.ts
+++ App/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CitySearchComponent } from './pages/city-search/city-search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CitySearchComponent
   ],
   imports: [
     BrowserModule,
